5178b037dff42cb3e367bef767b19055d7dd8b98
kieren millar <kieren_1@hotmail.com>
2015-04-19 23:07:00 +0100

    added main menu and finished quiz playing

--- serverLaunchers/PlayerClientLauncher.java
+++ serverLaunchers/PlayerClientLauncher.java
@@ -5,8 +5,11 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.LocalTime;
 import java.util.Scanner;
 
+import quiz.interfaces.Answer;
+import quiz.interfaces.Question;
 import quiz.interfaces.QuizService2;
 import quiz.interfaces.User;
 import quiz.interfaces.UserService2;
@@ -39,9 +42,27 @@ public class PlayerClientLauncher {
 		loadMainMenu();
 	}
 	
-	private void loadMainMenu() throws MalformedURLException, RemoteException, NotBoundException {
+	private void loadMainMenu() throws MalformedURLException, RemoteException, NotBoundException{
+		boolean finished = false;
+		do{
+			System.out.println("Welcome, what would you like to do:" + nl +
+					"1. Play Quizes" + nl + 
+					"0. LogOut");
+			switch (input.nextLine()){
+			case "0" : finished = true;
+				break;
+			case "1" : loadQuizMenu();
+				break;
+			default : System.out.println("invalid choice");
+				break;
+			}
+		}while(!finished);
+		System.exit(0);
+	}
+	
+	private void loadQuizMenu() throws MalformedURLException, RemoteException, NotBoundException {
 		QuizService2 qs = connectToQuizServer();
-		System.out.println("Welcome. Please select a quiz to play (1, 2, 3..." + nl);
+		System.out.println("Please select a quiz to play (1, 2, 3..." + nl);
 		int quizListSize = qs.getQuizs().size();
 		if (quizListSize > 0){
 			for (int i = 0; i < quizListSize; i++){
@@ -51,6 +72,41 @@ public class PlayerClientLauncher {
 			System.out.println("There are currently no active quizes");
 			System.exit(0);
 		}
+		int quizToPlay = 0;
+		do{
+			try{
+				quizToPlay = qs.checkIfActiveQuiz(input.nextLine());
+			}catch (IllegalArgumentException e){
+			System.out.println(e.getMessage());
+			}
+		}while (quizToPlay == 0);
+		playQuiz(quizToPlay);
+	}
+
+	private void playQuiz(int quizId) throws MalformedURLException, RemoteException, NotBoundException {
+		QuizService2 qs = connectToQuizServer();
+		int score = 0;
+		int numberOfQuestions = qs.getNumberOfQuestions(quizId, "activeQuizList");
+		int time = LocalTime.now().toSecondOfDay();
+		for(int i = 0; i < numberOfQuestions; i++){
+			Question tempQuestion = qs.getQuestion(quizId, i, "activeQuizList");
+			System.out.println(tempQuestion.getName() + nl);
+			for(Answer a : tempQuestion.getPossibleAnswers()){
+				System.out.println(a.getAnswerId() + ". " + a.getAnswer() + nl);
+			}
+			System.out.println("Please choose your answer (1, 2, 3, 4 or 5)");
+			boolean finished = false;
+			do{
+				try{
+					if (tempQuestion.getTrueAnswer() == Integer.parseInt(input.nextLine())) score++;
+					finished = true;
+				}catch (NumberFormatException e){
+					System.out.println("not a number");
+				}
+			}while(!finished);
+		}
+		time = LocalTime.now().toSecondOfDay() - time;
+		System.out.println("Your score was: " + qs.finishTakingQuiz(quizId, user, score, time));
 	}
 
 	private void loginUser() throws RemoteException, MalformedURLException, NotBoundException {
